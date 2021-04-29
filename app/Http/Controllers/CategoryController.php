1b7a71017ddd3b9d3f59a3cac291f47bae805bf0
alexxosipov <alexxosipov@yandex.ru>
2021-04-30 02:03:15 +0800

    init

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\News;
+
+class CategoryController extends Controller
+{
+    public function show(Category $category)
+    {
+        $news = $category->news()->get();
+
+        return view('categories.show', compact('category', 'news'));
+    }
+}
